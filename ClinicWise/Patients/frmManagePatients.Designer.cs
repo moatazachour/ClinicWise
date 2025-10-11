71c1cb6166f5f446854694ddec812d16d8cad539
moatazachour <moatazachour84@gmail.com>
2025-10-11 11:15:47 +0100

    Enhance patient management with filtering and UI improvements

--- ClinicWise/Patients/frmManagePatients.Designer.cs
+++ ClinicWise/Patients/frmManagePatients.Designer.cs
@@ -60,23 +60,25 @@
             this.cbManagePatients.FormattingEnabled = true;
             this.cbManagePatients.Items.AddRange(new object[] {
             "None",
-            "DoctorID",
+            "PatientID",
             "Fullname",
             "Gender",
             "Phone",
             "Email",
             "Address"});
-            this.cbManagePatients.Location = new System.Drawing.Point(211, 280);
-            this.cbManagePatients.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbManagePatients.Location = new System.Drawing.Point(158, 228);
+            this.cbManagePatients.Margin = new System.Windows.Forms.Padding(2);
             this.cbManagePatients.Name = "cbManagePatients";
-            this.cbManagePatients.Size = new System.Drawing.Size(217, 27);
+            this.cbManagePatients.Size = new System.Drawing.Size(164, 27);
             this.cbManagePatients.TabIndex = 22;
+            this.cbManagePatients.SelectedIndexChanged += new System.EventHandler(this.cbManagePatients_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(69, 283);
+            this.label3.Location = new System.Drawing.Point(52, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 23);
             this.label3.TabIndex = 21;
@@ -108,8 +110,8 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvManagePatients.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvManagePatients.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dgvManagePatients.Location = new System.Drawing.Point(37, 337);
-            this.dgvManagePatients.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvManagePatients.Location = new System.Drawing.Point(28, 274);
+            this.dgvManagePatients.Margin = new System.Windows.Forms.Padding(2);
             this.dgvManagePatients.MultiSelect = false;
             this.dgvManagePatients.Name = "dgvManagePatients";
             this.dgvManagePatients.ReadOnly = true;
@@ -124,7 +126,7 @@
             this.dgvManagePatients.RowHeadersWidth = 51;
             this.dgvManagePatients.RowTemplate.Height = 24;
             this.dgvManagePatients.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvManagePatients.Size = new System.Drawing.Size(1469, 410);
+            this.dgvManagePatients.Size = new System.Drawing.Size(1102, 333);
             this.dgvManagePatients.TabIndex = 20;
             this.dgvManagePatients.TabStop = false;
             // 
@@ -139,7 +141,7 @@
             this.updateToolStripMenuItem,
             this.deleteToolStripMenuItem});
             this.cmsManagePatients.Name = "cmsManageDoctors";
-            this.cmsManagePatients.Size = new System.Drawing.Size(217, 136);
+            this.cmsManagePatients.Size = new System.Drawing.Size(217, 114);
             // 
             // showDetailsToolStripMenuItem
             // 
@@ -177,10 +179,9 @@
             this.label1.BackColor = System.Drawing.Color.Azure;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(487, 134);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(365, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(591, 98);
+            this.label1.Size = new System.Drawing.Size(443, 80);
             this.label1.TabIndex = 18;
             this.label1.Text = "Manage Patients";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -188,10 +189,9 @@
             // btnAddPatient
             // 
             this.btnAddPatient.Image = global::ClinicWise.Properties.Resources.Add_Person_40;
-            this.btnAddPatient.Location = new System.Drawing.Point(1411, 262);
-            this.btnAddPatient.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddPatient.Location = new System.Drawing.Point(1058, 213);
             this.btnAddPatient.Name = "btnAddPatient";
-            this.btnAddPatient.Size = new System.Drawing.Size(95, 69);
+            this.btnAddPatient.Size = new System.Drawing.Size(71, 56);
             this.btnAddPatient.TabIndex = 27;
             this.btnAddPatient.UseVisualStyleBackColor = true;
             this.btnAddPatient.Click += new System.EventHandler(this.btnAddPatient_Click);
@@ -199,19 +199,20 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(1343, 766);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Location = new System.Drawing.Point(1007, 622);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(163, 52);
+            this.btnClose.Size = new System.Drawing.Size(122, 42);
             this.btnClose.TabIndex = 26;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // lblRecordCount
             // 
             this.lblRecordCount.AutoSize = true;
             this.lblRecordCount.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRecordCount.Location = new System.Drawing.Point(186, 766);
+            this.lblRecordCount.Location = new System.Drawing.Point(140, 622);
+            this.lblRecordCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRecordCount.Name = "lblRecordCount";
             this.lblRecordCount.Size = new System.Drawing.Size(21, 23);
             this.lblRecordCount.TabIndex = 25;
@@ -221,7 +222,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(32, 766);
+            this.label2.Location = new System.Drawing.Point(24, 622);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 23);
             this.label2.TabIndex = 24;
@@ -230,30 +232,30 @@
             // mtxtFilter
             // 
             this.mtxtFilter.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtxtFilter.Location = new System.Drawing.Point(463, 280);
-            this.mtxtFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.mtxtFilter.Location = new System.Drawing.Point(347, 228);
             this.mtxtFilter.Name = "mtxtFilter";
-            this.mtxtFilter.Size = new System.Drawing.Size(472, 27);
+            this.mtxtFilter.Size = new System.Drawing.Size(355, 27);
             this.mtxtFilter.TabIndex = 23;
+            this.mtxtFilter.TextChanged += new System.EventHandler(this.mtxtFilter_TextChanged);
+            this.mtxtFilter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mtxtFilter_KeyPress);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = global::ClinicWise.Properties.Resources.patient_care_100;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1567, 118);
+            this.pictureBox1.Size = new System.Drawing.Size(1175, 96);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 19;
             this.pictureBox1.TabStop = false;
             // 
             // frmManagePatients
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1567, 840);
+            this.ClientSize = new System.Drawing.Size(1175, 682);
             this.Controls.Add(this.cbManagePatients);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dgvManagePatients);
@@ -265,6 +267,7 @@
             this.Controls.Add(this.mtxtFilter);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmManagePatients";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Manage Patients";
--- ClinicWise/Patients/frmManagePatients.cs
+++ ClinicWise/Patients/frmManagePatients.cs
@@ -14,7 +14,9 @@ namespace ClinicWise.Patients
 {
     public partial class frmManagePatients : Form
     {
-        private List<PatientDisplayDTO> _PartientList;
+        private List<PatientDisplayDTO> _PatientList;
+
+        private List<PatientDisplayDTO> _PatientFilter;
 
         public frmManagePatients()
         {
@@ -24,8 +26,8 @@ namespace ClinicWise.Patients
         private async Task _RefreshData()
         {
             cbManagePatients.SelectedItem = "None";
-            _PartientList = await clsPatient.GetAllAsync();
-            dgvManagePatients.DataSource = _PartientList;
+            _PatientList = await clsPatient.GetAllAsync();
+            dgvManagePatients.DataSource = _PatientList;
 
             lblRecordCount.Text = dgvManagePatients.RowCount.ToString();
 
@@ -63,6 +65,73 @@ namespace ClinicWise.Patients
             frm.ShowDialog();
 
             await _RefreshData();
+        }
+
+        private void mtxtFilter_TextChanged(object sender, EventArgs e)
+        {
+            switch (cbManagePatients.Text)
+            {
+                case "PatientID":
+                    if (string.IsNullOrWhiteSpace(mtxtFilter.Text))
+                        _PatientFilter = _PatientList;
+                    else
+                        _PatientFilter = _PatientList.Where(p => p.PatientID == Convert.ToInt32(mtxtFilter.Text.Trim())).ToList();
+                    break;
+
+                case "Fullname":
+                    _PatientFilter = _PatientList.Where(p => p.FullName.ToLower().Contains(mtxtFilter.Text.ToLower().Trim())).ToList();
+                    break;
+
+                case "Gender":
+                    _PatientFilter = _PatientList.Where(p => p.GenderCaption.ToLower().StartsWith(mtxtFilter.Text.ToLower().Trim())).ToList();
+                    break;
+
+                case "Phone":
+                    _PatientFilter = _PatientList.Where(p => p.Phone.StartsWith(mtxtFilter.Text.Trim())).ToList();
+                    break;
+
+                case "Email":
+                    _PatientFilter = _PatientList.Where(p => p.Email.ToLower().StartsWith(mtxtFilter.Text.ToLower().Trim())).ToList();
+                    break;
+
+                case "Address":
+                    _PatientFilter = _PatientList.Where(p => p.Address != null &&
+                                                             p.Address.ToLower().StartsWith(mtxtFilter.Text.ToLower().Trim())).ToList();
+                    break;
+
+                default:
+                    _PatientFilter = _PatientList;
+                    break;
+            }
+
+            dgvManagePatients.DataSource = _PatientFilter;
+            lblRecordCount.Text = dgvManagePatients.RowCount.ToString();
+        }
+
+        private void mtxtFilter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbManagePatients.Text == "PatientID")
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void cbManagePatients_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mtxtFilter.Visible = (cbManagePatients.Text != "None");
+
+            if (mtxtFilter.Visible)
+            {
+                mtxtFilter.Mask = "";
+                mtxtFilter.Text = "";
+                mtxtFilter.Focus();
+            }
+
+            if (cbManagePatients.Text == "None")
+                dgvManagePatients.DataSource = _PatientList;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
