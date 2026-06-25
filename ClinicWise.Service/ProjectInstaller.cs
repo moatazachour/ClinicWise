389f92e953b62fa601c332e5a4e508c3ab33dec1
moatazachour <moatazachour84@gmail.com>
2026-06-25 18:49:01 +0100

    Add Windows Service installer and update project settings
    - Added ProjectInstaller to enable service installation for ClinicWise.Service.
    - Configured service to run as LocalSystem and start automatically.

---
+++ ClinicWise.Service/ProjectInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace ClinicWise.Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller serviceProcessInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public ProjectInstaller()
+        {
+            serviceProcessInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller.ServiceName = "ClinicWiseService";
+            serviceInstaller.DisplayName = "ClinicWise Background Service";
+            serviceInstaller.Description = "Handles automatic appointment reminders, no-show marking, and invoice payment reminders.";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
